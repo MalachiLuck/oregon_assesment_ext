ebf6bf67a56a05b78f0df636d745b2a44250f3a1
Malachi Luck <malachiluck@gmail.com>
2020-07-16 14:01:17 -0500

    fin

---
+++ wagon.js
@@ -0,0 +1,27 @@
+class Wagon {
+    constructor(capacity) {
+        this.passengers = []
+        this.capacity = capacity
+    }
+    getAvailableSeatCount() {
+        return this.capacity - this.passengers.length
+    }
+    join(Traveler) {
+        if (this.getAvailableSeatCount() >= 1) {
+            this.passengers.push(Traveler)
+        }
+    }
+    shouldQuarantine() {
+        for (let index = 0; index < this.passengers.length; index += 1) {
+            if (this.passengers[index].isHealthy === false)
+                return true
+        }
+    }
+    totalFood() {
+        let total = 0
+        for (let index= 0; index < this.passengers.length; index += 1) {
+            total += this.passengers[index].food 
+        }
+        return total
+    }
+}
